a052da5978a4db736b6a215122f4c66bfdab23e7
BugHunter-X <askzenai@gmail.com>
2024-12-11 05:38:10 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to access a property of a component's state before the component has mounted or after it has unmounted. This often happens when using asynchronous operations (like fetching data) within a component's lifecycle methods or event handlers.
